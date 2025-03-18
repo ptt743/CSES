81c9d666bd5f69c7c03d001d04c85d7313bc3f3d
taipt3 <taipt3@vng.com.vn>
2025-03-18 18:04:27 +0700

    update

---
+++ problems/Weird_Algorithm.cpp
@@ -0,0 +1,47 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+#include<string>
+#include<queue>
+#include<stack>
+#include<set>
+#include<unordered_map>
+#include<cmath>
+#include<functional>
+#define ll long long
+
+using namespace std;
+//*****taipt*****//
+/*
+*/
+void solve(){
+	long long n; cin>>n;
+	int res = 0;
+	function<void(int)> bfs=[&](int u){
+		queue<pair<int,int>> qq;
+		qq.push(make_pair(u,1));
+		set<int> visited;
+		visited.insert(u);
+		while(!qq.empty()){
+			pair<int,int> front = qq.front();
+			qq.pop();
+			int v = front.first;
+			int w = front.second;
+			if(v==1) {
+				res = w;
+				return;
+			}
+			if(v%2==0 && visited.find(v/2)==visited.end()) qq.push(make_pair(v/2,w+1));
+			if(v%2!=0 && visited.find(3*v+1)==visited.end()) qq.push(make_pair(3*v+1, w+1));
+		}
+	};
+		bfs(n);
+		cout<< res<<endl;
+	
+}
+ 
+int main() {
+    ios_base::sync_with_stdio(0); cin.tie(0);
+    solve();
+    return 0;
+}
